f55090d9081a9f8461044ad13ad271033a9afde2
lostsq <losthartsq@yahoo.com>
2016-05-30 21:41:50 -0700

    Started Label Work

---
+++ GTD_Player/Assets/Scripts/Gameplay/Generic/Text_Order.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Text_Render_Test : MonoBehaviour {
+
+	// Use this for initialization
+	     void Start () 
+     {
+        GetComponent<Renderer>().sortingOrder = 90;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
